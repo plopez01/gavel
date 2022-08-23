b2ca08dacae9ecec183b31160203f2a388de13cc
Plopez <iespaulopez@gmail.com>
2022-08-23 11:30:31 +0200

    Credentials implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,77 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as os from 'os';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gavel" is now active!');
+	context.subscriptions.push(
+		vscode.commands.registerCommand('gavel.start', async () => {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gavel.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Gavel!');
-	});
+			console.log(await getLoginInfo());
 
-	context.subscriptions.push(disposable);
+			// Create and show panel
+			const panel = vscode.window.createWebviewPanel(
+				'catCoding',
+				'Cat Coding',
+				vscode.ViewColumn.One,
+				{}
+			);
+
+			// And set its HTML content
+			panel.webview.html = getWebviewContent();
+		})
+	);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+async function getLoginInfo(){
+
+	const fs = require('fs');
+
+	try {
+		const data = fs.readFileSync(`${os.homedir()}/judge.session`, 'utf8');
+		let session = JSON.parse(data);
+		if(session){
+			return session; // If sesison already exists return
+		}
+	} catch (err) {
+		console.error(err);
+	}
+	
+	// If not then ask for credentials and save them at the user homedir
+	const username = await vscode.window.showInputBox({
+		ignoreFocusOut: true,
+		placeHolder: "user@example.com",
+		prompt: "jutge.org's username",
+	});
+
+	const password = await vscode.window.showInputBox({
+		ignoreFocusOut: true,
+		password: true,
+		placeHolder: "password",
+		prompt: "jutge.org's password",
+	});
+
+	console.log(username + " " + password);
+
+	fs.writeFile(`${os.homedir()}/judge.session`, JSON.stringify({username, password}), function(err: any) {
+		if(err) {
+			return console.log(err);
+		}
+		console.log("The file was saved!");
+	});
+
+	return {username, password}
+}
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
+}
